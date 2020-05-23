2a3362a783ff2c64a777aa9094f4e960deafdbe2
Harun <Master.harun@gmail.com>
2020-05-23 11:25:22 -0400

    Restructered constructer and added object declarations

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,7 +4,11 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
-    constructor(name: string, type:string, launchDate: string, orbitType: string, operational: boolean){
-        
-    }
+    constructor( name: string, type:string, launchDate: string, orbitType: string, operational: boolean){ 
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
+    };
 }
