0eba4d761be76bd21d3b14e359bd0a7cccee19c6
Harun <Master.harun@gmail.com>
2020-06-03 15:51:01 -0400

    new satellites from json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,24 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  showList: Satellite[];
   constructor() {
-    this.sourceList = [
-      new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-      new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-      new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-      new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-      new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+    this.showList = [];
+    window.fetch(satellitesUrl).then(function(response){
+      response.json().then(function(data){
+        
+        let fetchedSatellites = data.satellites;
+        let i: any;
+        for (fetchedSatellites of fetchedSatellites){
+          let satellite = new Satellite(fetchedSatellites.name, fetchedSatellites.type, fetchedSatellites.launchDate, fetchedSatellites.orbitalType, fetchedSatellites.operational);
+          this.sourceList.push(satellite)
+        }
+        this.showList = this.sourceList.slice(0)
+      }.bind(this));
+    }.bind(this));
+     
   }
 }
 
