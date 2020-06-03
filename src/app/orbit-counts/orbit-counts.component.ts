9a9c0221aa2ac86191453b009fc6e835a349258a
Harun <Master.harun@gmail.com>
2020-06-03 17:02:20 -0400

    Bonus Part 2

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-counts',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrbitCountsComponent implements OnInit {
 
+  @Input() satellites: Satellite[];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  count(type:string) {
+    let amt = 0;
+    for(let satellite of this.satellites) {
+      if(satellite.type.toLocaleLowerCase() === type.toLocaleLowerCase()){
+        amt ++;
+      }
+    }
+    return amt
+  }
 }
